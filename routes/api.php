c6ce3d79f67ab04ff43a6ab1edd4e8dd36d82011
Jose <josemj1997@gmail.com>
2026-01-24 09:18:02 +0100

    Cambiado el orden de las rutas /archivadas para que no haya conflictos con el id

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,11 @@ Route::get('/user', function (Request $request) {
 
 Route::get("/entregas", [EntregaController::class, "index"]);
 Route::post("/entregas", [EntregaController::class, "store"]);
+Route::get("/entregas/archivadas", [EntregaController::class, "obtenerEliminadas"]);
 Route::get("/entregas/{entrega}", [EntregaController::class, "show"]);
 Route::put("/entregas/{entrega}", [EntregaController::class, "update"]);
 Route::delete("/entregas/{entrega}", [EntregaController::class, "destroy"]);
 
-Route::get("/entregas/archivadas", [EntregaController::class, "obtenerEliminadas"]);
 Route::get("/entregas/archivadas/{id}", [EntregaController::class, "obtenerEntregaEliminada"]);
 
 Route::get('/entregas/pdf/{id}', [EntregaController::class, 'generarPdf']);
@@ -29,11 +29,11 @@ Route::get('/entregas/pdf/{id}', [EntregaController::class, 'generarPdf']);
 
 Route::get("/reservas", [ReservaController::class, "index"]);
 Route::post("/reservas", [ReservaController::class, "store"]);
+Route::get("/reservas/archivadas", [ReservaController::class, "obtenerEliminadas"]);
 Route::get("/reservas/{reserva}", [ReservaController::class, "show"]); 
 Route::put("/reservas/{reserva}", [ReservaController::class, "update"]);
 Route::delete("/reservas/{reserva}", [ReservaController::class, "destroy"]);
 
-Route::get("/reservas/archivadas", [ReservaController::class, "obtenerEliminadas"]);
 Route::get("/reservas/archivadas/{id}", [ReservaController::class, "obtenerReservaEliminada"]);
 Route::get('/reservas/pdf/{id}', [ReservaController::class, 'generarPdf']);
 
